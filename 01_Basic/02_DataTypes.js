d7705dedbaed71bd4f484495106b5afc0ae3d584
Krishna-Khadka <KrishKhadka2059@gmail.com>
2023-10-07 21:26:12 +0545

    DataType learnt

---
+++ 01_Basic/02_DataTypes.js
@@ -0,0 +1,19 @@
+
+// String
+// number
+// bigint
+// boolean
+// null
+// undefined
+// Symbol
+// object
+
+let name
+console.log(name);
+
+let age = null
+console.log(age);
+
+console.log(typeof name);
+
+console.log(typeof age);
