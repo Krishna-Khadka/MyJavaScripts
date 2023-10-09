fe450a5239e366dd807a3b91e17a0e51f46254b9
Krishna-Khadka <KrishKhadka2059@gmail.com>
2023-10-09 18:35:15 +0545

    String learnt

---
+++ 01_Basic/05_String.js
@@ -0,0 +1,31 @@
+
+const name = "Krishna"
+const age = 21
+
+
+// console.log(`Hello my name is ${name}. My age is ${age}`); //Always use backticks for string
+
+// new way to declare string
+
+let myName = new String("Krishna Khadka")
+
+// console.log(myName.length)
+
+// console.log(myName.toUpperCase())
+
+// console.log(myName.charAt(4));
+
+// console.log(myName.indexOf('r'))
+
+// console.log(myName.substring(0,5));
+
+// console.log(myName.slice(-15,10));
+
+// trim funstion is used to remove extra space
+
+const myUrl = "https://www.Krishna%20Khadka.com"
+// console.log(myUrl.replace('%20','-'));
+// console.log(myUrl.includes('Krishna'))
+
+// To split
+console.log(myUrl.split('%'));
