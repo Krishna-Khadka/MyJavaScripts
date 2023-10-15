89c2fbbb3dfd0914f2922ffb703986897a368659
Krishna-Khadka <KrishKhadka2059@gmail.com>
2023-10-15 08:34:20 +0545

    function basic 1

---
+++ 03_Basics/01_Function.js
@@ -0,0 +1,25 @@
+
+function greet() {
+    console.log("hello");
+    console.log("js");
+    console.log("practise");
+}
+//greet()
+
+function sum(num1, num2){
+    return num1+num2
+}
+const result = sum(2,2)
+// console.log(result);
+
+function isLogged(username){
+    if(!username){
+        console.log("Please enter a username");
+        return
+    }
+}
+console.log(isLogged());
+
+
+
+
