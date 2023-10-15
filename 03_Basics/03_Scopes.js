5a66722281600fc20163f70acdc0c745b407666a
Krishna-Khadka <KrishKhadka2059@gmail.com>
2023-10-15 10:02:41 +0545

    scopes in js

---
+++ 03_Basics/03_Scopes.js
@@ -0,0 +1,22 @@
+
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+var f = 50
+if(true){
+    let d = 10
+    const e = 20
+    var f = 30
+}
+
+// console.log(a);
+// console.log(b);
+console.log(f);
+
