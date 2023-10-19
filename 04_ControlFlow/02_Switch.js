32f707ba9d59b5fb90ba145646a7f2278330184a
Krishna-Khadka <KrishKhadka2059@gmail.com>
2023-10-19 15:32:39 +0545

    Switch Statement Learnt

---
+++ 04_ControlFlow/02_Switch.js
@@ -0,0 +1,16 @@
+
+const animal = "dog"
+switch (animal) {
+    case "cat":
+        console.log("I love Cat")
+        break;
+    case "dog":
+        console.log("I love Dog")
+        break;
+    case "horse":
+        console.log("I love Horse")
+        break;
+    default:
+        console.log("I am not animal lover")
+        break;
+}
