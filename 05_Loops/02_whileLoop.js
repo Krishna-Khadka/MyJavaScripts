f52f1dc636c6a963c679c9c43c0aec484570848f
Krishna-Khadka <KrishKhadka2059@gmail.com>
2023-10-19 17:14:39 +0545

    while and do while loops basics

---
+++ 05_Loops/02_whileLoop.js
@@ -0,0 +1,22 @@
+
+// while loop
+let i=1
+while (i<=10) {
+    console.log(`The value of i is ${i}`);
+    i = i + 1
+}
+
+// Array
+let myHero = ['spiderman','superman','batman'];
+let myArray = 0;
+while (myArray<myHero.length) {
+    console.log(`Superhero is ${myHero[myArray]}`);
+    myArray++
+}
+
+// do while loop
+let index =5
+do {
+    console.log(`The value of i is ${index}`);
+    index++
+} while (index<=15);
